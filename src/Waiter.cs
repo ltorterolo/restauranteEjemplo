78a9eda3953338dbb3bc8b6fcbbc940fb739292c
somazzilli <sofia.mazzilli@correo.ucu.edu.uy>
2025-09-01 10:42:29 -0300

    mozo

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,6 +7,22 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
+        public string Name { get; set; }
         private List<Table> assignedTables = new List<Table>();
+
+        public Waiter(string name)
+        {
+            Name = name;
+        }
+        public void AssignTable(Table mesa)
+        {
+            assignedTables.Add(mesa);
+        }
+
+        public void TakeOrder(Table mesa, Dish plato)
+        {
+            if (assignedTables.Contains(mesa))
+                mesa.AddDish(plato);
+        }
     }
 }
