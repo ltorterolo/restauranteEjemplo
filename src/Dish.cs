1b89f061ca2656811e11d37ed6647f4fa3920b5b
franciscamartin <francisca.martin@correo.ucu.edu.uy>
2025-09-01 10:19:35 -0300

    clase Dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,10 +1,19 @@
 namespace Ucu.Poo.Restaurant
+// clase Dish, no depende de ninguna otra
 {
-
-    /// <summary>
-    /// Representa un platillo individual en el menú del restaurante.
-    /// </summary>
     public class Dish
     {
+        public Dish(string name, double price, bool isVegetarian)
+        { 
+            this.Name = name;
+            this.Price = price; 
+            this.IsVegetarian = isVegetarian;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsVegetarian { get; set; }
     }
 }
+
+
