61c941a698e609d16edea59f5004a353f9e5659b
Banda <alebandaber@gmail.com>
2025-09-03 08:31:08 -0300

    version2

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,6 +7,23 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
-        private List<Table> assignedTables = new List<Table>();
+        private List<Table>assignedTables = new List<Table>();
+
+        public string Name { get; set; }
+
+        public Waiter(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AssignTable(Table table)
+        {
+            this.assignedTables.Add(table);
+        }
+
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.AddToOrder(dish);
+        }
     }
 }
